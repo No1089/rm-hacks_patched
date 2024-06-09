b6ab800d11ebe6410fc9f98c204cf8fb1d51ebac
Chris Fourie <8755037+No1089@users.noreply.github.com>
2024-06-09 15:23:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ NOCOLOR='\e[0m'
 
 APP_BINARY="/usr/bin/xochitl"
 CACHE_DIR="/home/root/.cache/remarkable/xochitl/qmlcache/"
-PATCH_URL="https://raw.githubusercontent.com/mb1986/rm-hacks/main/patches/"
+PATCH_URL="https://raw.githubusercontent.com/No1089/rm-hacks/main/patches/"
 ZONEINFO_DIR="/usr/share/zoneinfo/"
 
 WGET="wget"
@@ -140,7 +140,7 @@ patch () {
     echo -e "${COLOR_SUCCESS}Trying to download and install patch: '$patch_version'${NOCOLOR}"
 
     pass=$(sha256sum $APP_BINARY | cut -c1-64)
-    $WGET -cO- $PATCH_URL/${patch_version}_$hash.patch | openssl aes-256-cbc -d -a -md sha512 -pbkdf2 -iter 1000000 -salt -pass pass:$pass | tar -xjC $CACHE_DIR
+    $WGET -cO- https://raw.githubusercontent.com/mb1986/rm-hacks/main/patches/0.0.10_060c149b482d510091609949f27f9cf2ef0d4972.patch | openssl aes-256-cbc -d -a -md sha512 -pbkdf2 -iter 1000000 -salt -pass pass:$pass | tar -xjC $CACHE_DIR
 
     mkdir -p /etc/systemd/system/xochitl.service.d
     cat << EOF > /etc/systemd/system/xochitl.service.d/qmlfileops.conf
